9fdce15c22bbe225f3e45c2d0ad28ba0e465e32d
fjurcev <fjurcev@gmail.com>
2016-06-02 10:44:17 +0200

    Add files via upload

---
+++ src/components/List.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [ {"id":1, "text":"nesto"},
+					{"id":2, "text":"nesto2"},
+					{"id":3, "text":"nesto3"} 
+				  ];
+
+var List = React.createClass({
+	render: function() {
+		var listItems = ingredients.map(function(item) {
+			return <ListItem key={item.id} ingredient={item.text} />;
+		});
+		return (<ul>{listItems}</ul>);
+	}
+
+});
+
+module.exports = List;
